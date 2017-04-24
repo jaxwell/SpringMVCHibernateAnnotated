88694802ca526586fd8e58ba3e2599a07df7ef1a
Ievgen Bykov <ievgen.bykov@gmail.com>
2017-04-24 21:24:31 +0300

    Added Service Layer.

---
+++ src/main/java/net/bykov/springmvc/service/EmployeeService.java
@@ -0,0 +1,24 @@
+package net.bykov.springmvc.service;
+
+import net.bykov.springmvc.model.Employee;
+
+import java.util.List;
+
+
+public interface EmployeeService {
+
+    Employee findById(int id);
+
+    void saveEmployee(Employee employee);
+
+    void updateEmployee(Employee employee);
+
+    void deleteEmployeeBySsn(String ssn);
+
+    List<Employee> findAllEmployees();
+
+    Employee findEmployeeBySsn(String ssn);
+
+    boolean isEmployeeSsnUnique(Integer id, String ssn);
+
+}
